8f5da8854005fba734e532b1af8df0aa4b1566d1
Maikel Steneker <maikel.steneker@gmail.com>
2015-02-01 18:43:15 +0100

    Now using GTK+ for GUI.

--- src/gui/MainFrame.java
+++ src/gui/MainFrame.java
@@ -143,7 +143,7 @@ public class MainFrame extends javax.swing.JFrame {
          */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
+                if ("GTK+".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
                 }
